SVM now fixed averaging ~98% accuracy on CSV data.

tester_scikit_learn_svm.py
@@ -2,22 +2,12 @@
 # Testing use of scikit-learn's svm function
 
 import numpy as np
-#from sklearn import svm
-from sklearn.svm import SVC
+from sklearn import svm
+#from sklearn.svm import SVC
 import csv
 import sys
 import random
 
-# X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
-# y = np.array([1, 1, 2, 2])
-# from sklearn.svm import SVC
-# clf = SVC()
-# clf.fit(X, y)
-# SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-#     decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
-#     max_iter=-1, probability=False, random_state=None, shrinking=True,
-#     tol=0.001, verbose=False)
-# print(clf.predict([[-0.8, -1]]))
 
 rawdata = [] # 1st bone data, 2nd rock data
 traindata = []
@@ -28,7 +18,7 @@ testresults = [] # Output
 
 
 # Read in training data
-def load_images(filename):
+def load_csv(filename):
     print(filename + "\n")
     #print(os.path.join(path, filename) + "\n")
     with open(filename, 'r') as csvfile:
@@ -39,17 +29,13 @@ def load_images(filename):
 
 
 print(sys.argv[1] + "\n")
-load_images(sys.argv[1])
-print(rawdata)
-print("Finished 1\n")
+load_csv(sys.argv[1])
 
 new_data = []
 for x in rawdata:
     if x:
         new_data.append(x)
 rawdata = new_data
-
-print("Finished 1.5\n")
 
 # Split into training and testing data, 50:50 ratio
 datasize = len(rawdata)
@@ -63,35 +49,41 @@ for i in range(0, datasize):
         testdata.append(rawdata[i][0:3])
         testvalues.append(rawdata[i][3])
 
+
+
+
+
+print("TRAIN DATA:\n")
 print(traindata)
 print(trainvalues)
-print("Finished 2\n")
+print("TEST DATA:\n")
+print(testdata)
+print(testvalues)
 
-# train svm
+# train and test svm
 
-clf = SVC()
+
+clf = svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+    decision_function_shape='ovr', degree=3, gamma='auto', kernel='poly',
+    max_iter=-1, probability=False, random_state=None, shrinking=True,
+    tol=0.001, verbose=False)
+
 clf.fit(traindata, trainvalues)
 
-
-SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-     decision_function_shape='ovr', gamma='auto', kernel='poly',
-     max_iter=-1, probability=False, random_state=None, shrinking=True,
-     tol=0.001, verbose=False)
-
-print("Finished 3\n")
-
 result = clf.predict(testdata)
-print("RESULT:\n")
+
+# Print results
+
+print("PREDICTION:\n")
 print(len(result))
 print("\n")
 print(result)
-print("RESULT:\n")
+print("ACTUAL:\n")
 print(testvalues)
 print("\n")
 print(len(testvalues))
 print("\n")
 
-print("Finished 4\n")
 
 correct = 0
 for a in range(0, len(testvalues)):